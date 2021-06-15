7c159a920b9461891bb663aec3da5bb7cd176fa0
Sheela Adhikari <adhikarisheela1000@gmail.com>
2021-06-15 12:27:10 +0545

    add image

--- index1.js
+++ index1.js
@@ -1,12 +1,12 @@
 // alert("hello world");
-let login = 1;
-if (login == 0) {
-    console.log("go");
+// let login = 1;
+// if (login == 0) {
+//     console.log("go");
 
-}
+// }
 
 
-let age = 16;
-if (age == 16) {
-    console.log("you can vote")
-}
+// let age = 16;
+// if (age == 16) {
+//     console.log("you can vote")
+// }
